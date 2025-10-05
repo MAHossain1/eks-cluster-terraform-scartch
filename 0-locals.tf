cd440f117209e792146d17ee7997b8f46ffa7459
MAHossain1 <mahpro110@gmail.com>
2025-10-05 20:49:45 +0600

    network portion done.

---
+++ 0-locals.tf
@@ -0,0 +1,10 @@
+locals {
+  env         = "staging"
+  region      = "us-west-2"
+  zone1       = "us-west-2a"
+  zone2       = "us-west-2b"
+  eks_name    = "my-eks-cluster"
+  eks_version = "1.33"
+}
+
+
