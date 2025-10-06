81c0235172358a57e10545cc83e53bc8e55cdb42
MAHossain1 <mahpro110@gmail.com>
2025-10-06 07:35:17 +0600

    eks cluster protion done.

--- 7-eks.tf
+++ 7-eks.tf
@@ -1,1 +1,55 @@
+resource "aws_iam_role" "eks" {
+  name = "${local.env}-${local.eks_name}-eks-cluster-role"
 
+  assume_role_policy = <<POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"    
+            }
+        ]
+    }
+    POLICY
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
+resource "aws_eks_cluster" "eks" {
+  name     = "${local.env}-${local.eks_name}"
+  role_arn = aws_iam_role.eks.arn
+  version  = local.eks_version
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.private_zone1.id,
+      aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks
+  ]
+
+  tags = {
+    Name = "${local.env}-${local.eks_name}"
+  }
+
+}
